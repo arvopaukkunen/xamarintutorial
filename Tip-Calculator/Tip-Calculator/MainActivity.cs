b526bf696865aea084db75e975177ede7dc1159b
Arvo Paukkunen <arvosoft@hotmail.com>
2017-03-15 08:42:48 +0200

    sqlite and c++ ref for uwp

---
+++ Tip-Calculator/Tip-Calculator/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Tip_Calculator
+{
+    [Activity(Label = "Tip_Calculator", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
